cb754bcfac87b806fe90f207f8fb0c53cbb9dd77
Aleksandr <zaytsev.chel@gmail.com>
2023-03-05 04:19:19 +0500

    Домашка

---
+++ Example003/Program.cs
@@ -0,0 +1,56 @@
+﻿// // Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// // 44 -> 101100
+// // 3  -> 11
+// // 2  -> 10
+
+// int ReadInt(string promt)
+// {
+//     System.Console.WriteLine(promt);
+//     return Convert.ToInt32(Console.ReadLine());
+// }
+
+
+// int[] ConverToBinary(int value)
+// {
+//     int count = 0;
+//     int value2 = value;
+//     while (value2 > 0)
+//     {
+//         value /= 2;
+//         count++;
+//     }
+//     int[] result = new int[count];
+//     for (int i = 0; i < count; i++)
+//     {
+//         result[i] = value2%2;
+//         value /=2;
+//     }
+//     Array.Reverse(result);
+//     return result;
+// }
+
+// void PrintArray(int[] array)
+// {
+//     System.Console.WriteLine(string.Join("", array));
+// }
+
+// int num = ReadInt("Введите число: ");
+// int[] binaryNum = ConverToBinary(num);
+// PrintArray(binaryNum);
+
+
+int ReadInt(string text)
+{
+    System.Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void GetBinaryView(int num)
+{
+    if (num == 0) return;
+    GetBinaryView(num / 2);
+    System.Console.Write(num % 2);
+}
+
+int N = ReadInt("Введите число");
+GetBinaryView(N);
---
+++ HW1/Program.cs
@@ -0,0 +1,49 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+}
+
+int[] LengthArray(int length)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < array.Length; i++)
+    {
+       array[i] = ReadInt ($"Введите {i + 1}-й элемент: ");
+    }
+    return array;
+}
+
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.WriteLine($"а[{i}] = {array[i]}");
+    }
+}
+
+int CountPositiveNumbers(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int length = ReadInt("Введите количество элементов >");
+int[] array;
+array = LengthArray(length);
+PrintArray(array);
+System.Console.WriteLine($"Количество чисел больше 0 - {CountPositiveNumbers(array)}");
